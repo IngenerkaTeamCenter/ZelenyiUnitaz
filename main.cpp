28b901dcb489a5269e06e620f7e9b9c8d6ee5247
Arsenee <32013422+Arsenee@users.noreply.github.com>
2017-10-24 17:04:12 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,11 +17,12 @@ struct Mebel {
     int v;   //âèä îáúåêòà (êðàñíûé øêàô, ñëîìàííûé øêàô)
 };
 
-struct Knopka{
+struct Knopka {
     int x;
     int y;
     const char* text;
-
+    int risovatKartinku;
+    HDC kartinka;
 };
 
 const int GAME_MODE = 1;
@@ -29,24 +30,24 @@ const int REDACTOR_MODE = 100;
 
 int mode = GAME_MODE;
 
-void draw_button(Knopka k)
+void draw_button(Knopka knopki_dlya_menu)
 {
     txSetColour(RGB(0, 0, 0), 1);
     txSetFillColour(RGB(255, 255, 255));
     txSelectFont("Times New Roman", 25);
-    txRectangle(k.x,k.y,k.x+400,k.y+100);
-    txTextOut(k.x, k.y + 40, k.text);
+    txRectangle(knopki_dlya_menu.x,knopki_dlya_menu.y,knopki_dlya_menu.x+300,knopki_dlya_menu.y+100);
+    txTextOut(knopki_dlya_menu.x, knopki_dlya_menu.y + 40, knopki_dlya_menu.text);
 }
 
-void btn_click (int x, int y, int xx, int yy, int risovatKartinku)
+void btn_click (Knopka* knopki_dlya_menu)
 {
-    if (txMouseButtons() == 1
-        && txMouseX() >= x
-        && txMouseX() <= y
-        && txMouseY() >= xx
-        && txMouseY() <= yy)
+    if (txMouseButtons() & 1
+        && txMouseX() >= knopki_dlya_menu->x
+        && txMouseX() <= knopki_dlya_menu->x + 300
+        && txMouseY() >= knopki_dlya_menu->y
+        && txMouseY() <= knopki_dlya_menu->y + 100)
     {
-        risovatKartinku = -risovatKartinku;
+        knopki_dlya_menu->risovatKartinku = -knopki_dlya_menu->risovatKartinku;
         txSleep(100);
     }
 }
@@ -55,10 +56,10 @@ int main()
 {
     txCreateWindow(800,720);
 
-    int risovatKartinku = -1;
-    HDC tv = txLoadImage ("Icons\\òåëåâèçîð.bmp");
+    HDC tv = txLoadImage ("Icons\\tv.bmp");
 
-    Knopka k = {0, 0, "Òåëåâèçîð"};
+    Knopka knopki_dlya_menu[10];
+    knopki_dlya_menu[0] = {0, 0, "TV", -1, tv};
 
     while (!GetAsyncKeyState(VK_ESCAPE))
     {
@@ -69,19 +70,26 @@ int main()
         txSetColour(RGB(0, 0, 0), 1);
         txSetFillColour(RGB(255, 255, 255));
         txSelectFont("Times New Roman", 25);
-        draw_button(k);
-        
-        btn_click (0,200,0,100, risovatKartinku);      //0 200 0 100
 
-        if (risovatKartinku == 1)
+        for (int nomer_knopki = 0; nomer_knopki < 1; nomer_knopki++)
         {
-            txBitBlt (txDC(), 500, 500, 30, 30, tv, 0, 0);
+            draw_button(knopki_dlya_menu[nomer_knopki]);
+            btn_click (&knopki_dlya_menu[nomer_knopki]);
+        }
+
+        if (knopki_dlya_menu[0].risovatKartinku == 1)
+        {
+            txBitBlt (txDC(), 500, 500, 30, 30, knopki_dlya_menu[0].kartinka, 0, 0);
         }
 
         txSleep (10);
         txEnd();
     }
 
+    for (int nomer_knopki = 0; nomer_knopki < 1; nomer_knopki++)
+    {
+        txDeleteDC (knopki_dlya_menu[nomer_knopki].kartinka);
+    }
     txDeleteDC (tv);
 
     return 0;
