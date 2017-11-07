920d79c1991614e273f9bf970e0700d85268bb3f
Nikita <silantyevnikitaaa@gmail.com>
2017-11-07 16:34:17 +0400

    Добавил библиотеку, стало намного интереснее играт
    Если вы играли с одной цифрой, теперь их 5!!!!!

---
+++ Lib/Menu.cpp
@@ -0,0 +1,53 @@
+struct Knopka {
+    int x;
+    int y;
+    const char* text;
+    int risovatKartinku;
+    HDC kartinka;
+};
+
+Knopka knopki_dlya_menu[KOLVO_KNOPOK];
+
+void Level_1 (HDC TV, HDC Chair, HDC Table, HDC Bed)
+{
+      txBitBlt (txDC(), 200, 200, 30, 30, TV);
+      txBitBlt (txDC(), 200, 400, 30, 30, TV);
+      txBitBlt (txDC(), 400, 200, 30, 30, TV);
+      txBitBlt (txDC(), 300, 200, 30, 30, Chair);
+      txBitBlt (txDC(), 200, 300, 30, 30, Chair);
+      txBitBlt (txDC(), 300, 300, 30, 30, Chair);
+      txBitBlt (txDC(), 600, 200, 30, 30, Table);
+}
+
+
+void draw_button(Knopka knop)
+{
+    txSetColour(RGB(0, 0, 0), 1);
+    txSetFillColour(RGB( 195, 195, 195));
+    txSelectFont("Times New Roman", 25);
+    txRectangle(knop.x,knop.y,knop.x+200,knop.y+100);
+    txDrawText(knop.x, knop.y + 40, knop.x + 200, knop.y + 100, knop.text, DT_CENTER);
+}
+
+void btn_click (Knopka* knop)
+{
+    if (txMouseButtons() & 1
+        && txMouseX() >= knop->x
+        && txMouseX() <= knop->x + 200
+        && txMouseY() >= knop->y
+        && txMouseY() <= knop->y + 100)
+    {
+        knop->risovatKartinku = -knop->risovatKartinku;
+        txSleep(100);
+    }
+}
+
+void zap()
+{
+    knopki_dlya_menu[0] = {0, 0, "vanna", -1, txLoadImage ("Icons\\tv.bmp")};
+    knopki_dlya_menu[1] = {200, 0, "zal", -1, txLoadImage ("Icons\\technology\\PC_1.bmp")};
+    knopki_dlya_menu[2] = {400, 0, "kuxnua", -1, txLoadImage ("Icons\\table.bmp")};
+    knopki_dlya_menu[3] = {600, 0, "spalnua", -1, txLoadImage ("Icons\\bad.bmp")};
+    knopki_dlya_menu[4] = {800, 0, "spalnua", -1, txLoadImage ("Icons\\iPod_nano_6.bmp")};
+}
+
--- Lib/lol.cpp
+++ Lib/lol.cpp
@@ -4,39 +4,5 @@ struct Mebel {
     //int o;   //òèï îáúåêòà (øêàô, ñòîë)
     //int v;   //âèä îáúåêòà (êðàñíûé øêàô, ñëîìàííûé øêàô)
     int shirina;
-    HDC kartinka;
     int width;
 };
-
-struct Knopka {
-    int x;
-    int y;
-    const char* text;
-    int risovatKartinku;
-    HDC kartinka;
-};
-
-const int GAME_MODE = 1;
-const int REDACTOR_MODE = 100;
-
-void draw_button(Knopka knop)
-{
-    txSetColour(RGB(0, 0, 0), 1);
-    txSetFillColour(RGB( 195, 195, 195));
-    txSelectFont("Times New Roman", 25);
-    txRectangle(knop.x,knop.y,knop.x+200,knop.y+100);
-    txDrawText(knop.x, knop.y + 40, knop.x + 200, knop.y + 100, knop.text, DT_CENTER);
-}
-
-void btn_click (Knopka* knop)
-{
-    if (txMouseButtons() & 1
-        && txMouseX() >= knop->x
-        && txMouseX() <= knop->x + 200
-        && txMouseY() >= knop->y
-        && txMouseY() <= knop->y + 100)
-    {
-        knop->risovatKartinku = -knop->risovatKartinku;
-        txSleep(100);
-    }
-}
